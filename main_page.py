10e5b64c005491e9188c4fbff2c62a3654dd1281
Hunter Rogers <hunterwr1997@gmail.com>
2022-10-04 10:23:35 -0600

    try

--- main_page.py
+++ main_page.py
@@ -46,11 +46,11 @@ st.write(st.session_state['opt'])
 
 options = st.session_state['opt']
 for row in options.iterrows():
-    strike = row['Strike']
-    option_type = row['Type']
-    buysell = row['Direction']
-    commission = row['Commission']
-    name = row['Name']
+    strike = row[1]['Strike']
+    option_type = row[1]['Type']
+    buysell = row[1]['Direction']
+    commission = row[1]['Commission']
+    name = row[1]['Name']
 
     center = round(st.session_state['opt']['Strike'].mean(), 0)
 
