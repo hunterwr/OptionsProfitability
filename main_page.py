e7fb624b80acd436be043a64373b56c6f399519e
hunterwr <rog19011@byui.edu>
2022-10-03 21:34:47 -0600

    yi

--- main_page.py
+++ main_page.py
@@ -4,10 +4,22 @@ import altair as alt
 
 st.markdown('# Option Profitability')
 
-col1, col2, col3, col4 = st.columns([1,1,1,1])
+col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1])
 
 tickr = col1.text_input('Ticker')
 strike = col2.number_input('Strike Price')
-option_type = col3._selectbox('Call or Put', ['Call', 'Put'])
-buysell = col3._selectbox('Buy or Sell', ['Buy', 'Sell'])
+option_type = col3.selectbox('Call or Put', ['Call', 'Put'])
+buysell = col4.selectbox('Buy or Sell', ['Buy', 'Sell'])
+gap = col5.number_input('Gap between strikes')
+
+
+df = pd.DataFrame()
+
+for i in range(-10, 10):
+    #define row to add
+    row_to_append = pd.DataFrame([{'Expiration Price':(strike-(gap*i)), 'points':i}])
+
+    #add row to empty DataFrame
+    df = pd.concat([df, row_to_append])
+
 
