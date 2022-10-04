71f027dd9fb5c447a2bf9d8e913701e60ae8fd75
Hunter Rogers <hunterwr1997@gmail.com>
2022-10-04 12:45:35 -0600

    g

--- main_page.py
+++ main_page.py
@@ -78,14 +78,14 @@ else:
     pass
 
 if 'opt' in st.session_state:
-
     btn2 = col2.button("Clear list")
+    st.write(st.session_state['opt'])
     if btn2:
         del st.session_state['opt']
         btn = False
     else:
         pass
-    st.write(st.session_state['opt'])
+    
 
 
 
